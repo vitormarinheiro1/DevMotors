26e2594fca782ef1a0c4574618c5a3ea4e9441c5
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-07 10:35:32 -0300

    Adicionando Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div>
-        <h1>test</h1>
-      </div>
+      
     </main>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { useState, useEffect} from 'react'
 import Link from 'next/link'
 import styles from './styles.module.scss'
 
+
 export function Header() {
+    const [top, setTop] = useState(true);
+
+    const scrollHandler = () => {
+        window.scrollY > 10 ? setTop(false) : setTop(true)
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", scrollHandler)
+
+        return () => window.removeEventListener("scroll", scrollHandler)
+    }, [top])
+
     return (
-        <header className={styles.header}>
+        <header className={`${styles.header} ${!top ? styles.fixed : styles.background}`}>
             <div className={styles.container}>
                 <div className={styles.content}>
                     <div className={styles.contentLogo}>
@@ -11,6 +27,19 @@ export function Header() {
                             Dev Motors
                         </Link>
                     </div>
+
+                    <nav className={styles.nav}>
+                        <Link href="/">
+                            HOME
+                        </Link>
+                        <Link href="/#servicos">
+                            SERVIÇOS
+                        </Link>
+                        <Link href="/#contatos">
+                            CONTATOS
+                        </Link>
+                    </nav>
+
                 </div>
             </div>
         </header>
