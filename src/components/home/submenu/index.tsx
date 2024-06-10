708a4802a92144bbc4477309d42eeb9756fbd6d7
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-06-10 16:40:47 -0300

    Update styles submenu

--- src/components/home/submenu/index.tsx
+++ src/components/home/submenu/index.tsx
@@ -1,16 +1,43 @@
+"use client"
 import Link from 'next/link'
 import styles from './styles.module.scss'
-
-import { X, Menu} from 'lucide-react'
+import { useEffect, useState } from 'react'
+import { X, Menu } from 'lucide-react'
 
 export function Submenu() {
-    return(
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth > 768) {
+                setIsOpen(false);
+            }
+        }
+
+        window.addEventListener("resize", handleResize)
+
+        return () => window.removeEventListener("resize", handleResize)
+
+    }, [])
+
+    function toggleMenu() {
+        setIsOpen(!isOpen);
+    }
+
+
+    return (
         <section className={styles.submenu}>
-            <div className={styles.submenuIcon}>
-                <Menu size={34} color="#121212"/>
-                Menu
+            <div className={styles.submenuIcon} onClick={toggleMenu}>
+                <Menu size={34} color="#121212" />
+                Serviços
             </div>
-            <ul className={styles.ul}>
+            <ul className={`${styles.ul} ${isOpen ? styles.open : ""}`}>
+
+                {isOpen &&
+                    <button onChange={toggleMenu}>
+                        <X size={54} color='#121212' />
+                    </button>
+                }
                 <li>
                     <Link href="/post/pagina-1">
                         Pagina 1
